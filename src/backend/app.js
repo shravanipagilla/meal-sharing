92fb62c93af0270d617d93a5dbdc87f1e3efa47d
shravanipagilla <nampally04@gmail.com>
2022-09-25 14:16:54 +0200

    first commit

--- src/backend/app.js
+++ src/backend/app.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const path = require("path");
 
 const mealsRouter = require("./api/meals");
+const reviewsRouter = require("./api/reviews");
 const buildPath = path.join(__dirname, "../../dist");
 const port = process.env.PORT || 3000;
 const cors = require("cors");
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use(cors());
 
 router.use("/meals", mealsRouter);
+router.use("/reviews",reviewsRouter);
 
 if (process.env.API_PATH) {
   app.use(process.env.API_PATH, router);
@@ -31,5 +33,6 @@ if (process.env.API_PATH) {
 app.use("*", (req, res) => {
   res.sendFile(path.join(`${buildPath}/index.html`));
 });
+
 
 module.exports = app;
